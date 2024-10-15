5c4528e6c44fb9749f0a3ebd7972998463f06fa0
Kaushlendu-Narayan-Parashar <141625950+Kaushlendu-Narayan-Parashar@users.noreply.github.com>
2024-10-15 19:14:31 +0530

    Add files via upload

---
+++ on1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+df = pd.read_csv('rate.csv')
+
+plt.scatter(df['Fairness'], df['Rate'])
+plt.xlabel('Fairness')
+plt.ylabel('Rate')
+#plt.show()
+
+l_reg = linear_model.LinearRegression()
+l_reg.fit(df[['Fairness']], df.Rate)
+
+#print(l_reg.predict([[0]]))
+print(l_reg.coef_)
+print(l_reg.intercept_)
